8625fea56173d717495aab771b2f731c6d7a65e9
ceveloper7 <cevabarcvilla@gmail.com>
2026-04-03 02:36:13 +0000

    Databases - MYSQL fundaments

---
+++ mysql/Querying_mysql_ch0.sql
@@ -0,0 +1,10 @@
+Creacion de una tabla simple
+CREATE TABLE students(
+  student_id INT,
+  name VARCHAR(50),
+  age INT,
+  grade VARCHAR(2)
+);
+
+Verficar la structura de una table -> desc [nombre de tabla]
+
